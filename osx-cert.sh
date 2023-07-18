8c2559d581f90a859ac63918db598f54bd9d042a
mdwriter-09idI <mdwriter@163.com>
2023-07-18 22:29:37 +0800

    feat: ci-mas-beta13

--- osx-cert.sh
+++ osx-cert.sh
@@ -18,10 +18,18 @@ security unlock-keychain -p actions $KEY_CHAIN
 
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
-#security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
 # mas
 echo $CERTIFICATE_OSX_INSTALL | base64 --decode > $CERTIFICATE_INSTALL_P12
+
+security create-keychain -p actions $KEY_CHAIN
+
+# Make the keychain the default so identities are found
+security default-keychain -s $KEY_CHAIN
+
+# Unlock the keychain
+security unlock-keychain -p actions $KEY_CHAIN
 
 security import $CERTIFICATE_INSTALL_P12 -k $KEY_CHAIN -P $INSTALL_PASSWORD -T /usr/bin/codesign;
 
