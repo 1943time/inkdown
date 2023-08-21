eec17b8fc9599e30207574667deaa1678c4f5134
mdwriter-09idI <mdwriter@163.com>
2023-08-21 20:05:21 +0800

    feat: hijack copy text

--- src/renderer/src/editor/plugins/useKeyboard.ts
+++ src/renderer/src/editor/plugins/useKeyboard.ts
@@ -1,5 +1,5 @@
 import React, {useMemo} from 'react'
-import {Editor, Element, Node, Path, Range, Text, Transforms} from 'slate'
+import {Editor, Element, Node, Path, Point, Range, Text, Transforms} from 'slate'
 import {TabKey} from './hotKeyCommands/tab'
 import {EnterKey} from './hotKeyCommands/enter'
 import {BackspaceKey} from './hotKeyCommands/backspace'
@@ -7,7 +7,9 @@ import {MatchKey} from './hotKeyCommands/match'
 import {keyArrow} from './hotKeyCommands/arrow'
 import {isMix} from '../output'
 import {EditorUtils} from '../utils/editorUtils'
+import isHotkey from 'is-hotkey'
 
+const textNodes = new Set(['table-row', 'code-line', 'paragraph', 'head'])
 export const useKeyboard = (editor: Editor) => {
   return useMemo(() => {
     const tab = new TabKey(editor)
@@ -15,6 +17,29 @@ export const useKeyboard = (editor: Editor) => {
     const enter = new EnterKey(editor, backspace)
     const match = new MatchKey(editor)
     return (e: React.KeyboardEvent) => {
+      if (isHotkey('mod+c', e) && editor.selection) {
+        e.preventDefault()
+        const nodes = Editor.nodes(editor, {
+          match: n => Element.isElement(n) && textNodes.has(n.type)
+        })
+        let str:string[] = []
+        for (let [n, path] of nodes) {
+          const nodeRange = Range.edges(Editor.range(editor, path))
+          if (EditorUtils.includeAll(editor, editor.selection, path)) {
+            str.push(Node.string(n))
+          } else {
+            let [start, end] = Range.edges(editor.selection)
+            const compare = Point.compare(start, nodeRange[0])
+            if (compare === -1) start = nodeRange[0]
+            if (compare === 1) end = nodeRange[1]
+            str.push(Editor.string(editor, {
+              anchor: start,
+              focus: end
+            }))
+          }
+        }
+        window.api.copyToClipboard(str.join('\n'))
+      }
       // 防止在混合样式文字左右加入空格
       if (e.code === 'Space') {
         const [text] = Editor.nodes(editor, {
@@ -42,18 +67,16 @@ export const useKeyboard = (editor: Editor) => {
           }
         }
       }
-      if (e.metaKey && e.key === 'a') {
-        if (e.key === 'a' && editor.selection) {
-          const [code] = Editor.nodes(editor, {
-            match: n => Element.isElement(n) && n.type === 'code'
+      if (isHotkey('mod+a', e) && editor.selection) {
+        const [code] = Editor.nodes(editor, {
+          match: n => Element.isElement(n) && n.type === 'code'
+        })
+        if (code) {
+          Transforms.select(editor, {
+            anchor: Editor.start(editor, code[1]),
+            focus: Editor.end(editor, code[1])
           })
-          if (code) {
-            Transforms.select(editor, {
-              anchor: Editor.start(editor, code[1]),
-              focus: Editor.end(editor, code[1])
-            })
-            e.preventDefault()
-          }
+          e.preventDefault()
         }
       }
       match.run(e)
