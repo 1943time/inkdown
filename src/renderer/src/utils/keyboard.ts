37ac077eaf8a8182e7016bc66a2fa8a203f78d8e
mdwriter-09idI <mdwriter@163.com>
2023-11-30 16:43:28 +0800

    refactor: hotkey insert table

--- src/renderer/src/utils/keyboard.ts
+++ src/renderer/src/utils/keyboard.ts
@@ -180,7 +180,7 @@ export class MenuKey {
             Transforms.insertNodes(editor, {
               type: 'table',
               children: [
-                {type:'table-row', children: [{type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}]},
+                {type:'table-row', children: [{type: 'table-cell', title: true, children: [{text: ''}]}, {type: 'table-cell', title: true, children: [{text: ''}]}, {type: 'table-cell', title: true, children: [{text: ''}]}]},
                 {type:'table-row', children: [{type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}]},
                 {type:'table-row', children: [{type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}, {type: 'table-cell', children: [{text: ''}]}]}
               ]
