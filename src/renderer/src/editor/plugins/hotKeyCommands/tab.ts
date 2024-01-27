79346dd0a606cf3caca88ae0cd414f97e8af567f
mdwriter-09idI <mdwriter@163.com>
2024-01-27 17:57:37 +0800

    fix: #45 Pressing shift + tab in a list

--- src/renderer/src/editor/plugins/hotKeyCommands/tab.ts
+++ src/renderer/src/editor/plugins/hotKeyCommands/tab.ts
@@ -1,19 +1,22 @@
 import React from 'react'
-import {BasePoint, Editor, Element, Node, NodeEntry, Path, Point, Range, Transforms} from 'slate'
+import {BasePoint, Editor, Element, Node, NodeEntry, Path, Point, Range, Text, Transforms} from 'slate'
 import {CodeLineNode, NodeTypes, ParagraphNode, TableCellNode, TableRowNode} from '../../../el'
 import {configStore} from '../../../store/config'
+import {clearAllCodeCache} from '../useHighlight'
 
 export class TabKey {
   constructor(
     private readonly editor: Editor
-  ) {}
+  ) {
+  }
+
   run(e: React.KeyboardEvent) {
     const sel = this.editor.selection
     if (!sel) return
     e.preventDefault()
     if (Range.isCollapsed(sel)) {
       const [node] = Editor.nodes<any>(this.editor, {
-        match: n => Element.isElement(n) &&['table-cell', 'paragraph', 'code-line'].includes(n.type),
+        match: n => Element.isElement(n) && ['table-cell', 'paragraph', 'code-line'].includes(n.type),
         mode: 'lowest'
       })
       if (sel) {
@@ -24,8 +27,10 @@ export class TabKey {
               if (this.tableCell(el, path)) return
               break
             case 'paragraph':
-              const parent = Node.parent(this.editor, node[1])
-              if (parent.type === 'list-item') if (this.listItem(node)) return
+              const parent = Editor.parent(this.editor, node[1])
+              if (parent && parent[0].type === 'list-item') {
+                if (this.listItem(node, e)) return
+              }
               break
             case 'code-line':
               if (this.codeLine(e, node, sel)) return
@@ -33,7 +38,25 @@ export class TabKey {
           }
         }
       }
-      this.editor.insertText('\t')
+      if (e.shiftKey) {
+        const [leaf] = Editor.nodes(this.editor, {
+          match: n => Text.isText(n)
+        })
+        if (leaf) {
+          const str = Node.string(leaf[0])
+          if (str && /^\t/.test(str)) {
+            Transforms.insertText(this.editor, '', {
+              at: {
+                anchor: {path: leaf[1], offset: 0},
+                focus: {path: leaf[1], offset: 1}
+              }
+            })
+            Transforms.select(this.editor, {path: sel.anchor.path, offset: sel.anchor.offset - 1})
+          }
+        }
+      } else {
+        this.editor.insertText('\t')
+      }
     } else {
       const [start, end] = Range.edges(sel)
       const [code] = Editor.nodes(this.editor, {
@@ -76,6 +99,20 @@ export class TabKey {
           }
           return
         }
+      } else if (e.shiftKey) {
+        const [node] = Editor.nodes<any>(this.editor, {
+          match: n => n.type === 'list'
+        })
+        if (node) {
+          const [start, end] = Range.edges(sel)
+          const anchor = start.path.join(',').startsWith(node[1].join(',')) ? start : Editor.start(this.editor, node[1])
+          const focus = end.path.join(',').startsWith(node[1].join(',')) ? end : Editor.end(this.editor, node[1])
+          Transforms.liftNodes(this.editor, {at: {anchor, focus}})
+          Transforms.liftNodes(this.editor)
+        } else {
+          Transforms.liftNodes(this.editor)
+        }
+        return
       }
       Transforms.select(this.editor, {
         path: end.path,
@@ -83,6 +120,7 @@ export class TabKey {
       })
     }
   }
+
   private codeLine(e: React.KeyboardEvent, node: NodeEntry<CodeLineNode>, sel: Range) {
     if (e.shiftKey) {
       const str = Node.string(node[0])
@@ -102,7 +140,7 @@ export class TabKey {
             }
           }
         })
-        const point:BasePoint = {
+        const point: BasePoint = {
           path: sel.anchor.path,
           offset: sel.anchor.offset - length
         }
@@ -116,6 +154,7 @@ export class TabKey {
     }
     return true
   }
+
   private tableCell(node: TableCellNode, nodePath: Path) {
     const sel = this.editor.selection!
     const text = Node.string(node)
@@ -141,26 +180,63 @@ export class TabKey {
     return false
   }
 
-  private listItem(node: NodeEntry<ParagraphNode>) {
-    const listItem = Editor.parent(this.editor, node[1])
-    const list = Editor.parent(this.editor, listItem[1])
-    if (!Node.string(node[0]) && Path.hasPrevious(listItem[1]) && listItem[0].children.length === 1) {
-      Transforms.removeNodes(this.editor, {
-        at: listItem[1]
-      })
-      const pre = Path.previous(listItem[1])
-      const [next] = Node.children(this.editor, pre, {reverse: true})
-      Transforms.insertNodes(this.editor, {
-        type: 'list',
-        order: list[0].order,
-        children: [{
-          type: 'list-item',
-          checked: Node.get(this.editor, pre).checked,
-          children: [{type: 'paragraph', children: [{text: ''}]}]
-        }]
-      }, {at: Path.next(next[1]), select: true})
+  private listItem(node: NodeEntry<ParagraphNode>, e: React.KeyboardEvent) {
+    if (e.shiftKey) {
+      const li = Editor.node(this.editor, Path.parent(node[1]))
+      const ul = Editor.node(this.editor, Path.parent(li[1]))
+      const container = Editor.node(this.editor, Path.parent(ul[1]))
+      const top = !Path.hasPrevious(node[1])
+      const first = !Path.hasPrevious(li[1])
+      if (top) {
+        Transforms.liftNodes(this.editor, {at: li[1]})
+        if (container[0].type === 'list-item') {
+          const movePath = first ? ul[1] : Path.next(ul[1])
+          Transforms.moveNodes(this.editor, {at: movePath, to: Path.next(container[1])})
+          let start = li[0].children.length
+          while (Editor.hasPath(this.editor, movePath)) {
+            Transforms.moveNodes(this.editor, {
+              at: movePath,
+              to: [...Path.next(container[1]), start]
+            })
+            start++
+          }
+        } else {
+          Transforms.unwrapNodes(this.editor, {at: first ? ul[1] : Path.next(ul[1])})
+        }
+      } else {
+        const move = li[0].children.length - node[1].slice().pop()! - 1
+        Transforms.liftNodes(this.editor, {
+          at: {anchor: Editor.start(this.editor, node[1]), focus: Editor.end(this.editor, li[1])}
+        })
+        const nextPath = Path.next(li[1])
+        const lastIndex = nextPath.slice().pop()!
+        Transforms.liftNodes(this.editor, {
+          at: {anchor: {path: nextPath, offset: 1}, focus: {path: nextPath.slice(0, -1).concat([lastIndex + move]), offset: 0}}
+        })
+      }
+      clearAllCodeCache(this.editor)
       return true
+    } else {
+      const listItem = Editor.parent(this.editor, node[1])
+      const list = Editor.parent(this.editor, listItem[1])
+      if (!Node.string(node[0]) && Path.hasPrevious(listItem[1]) && listItem[0].children.length === 1) {
+        Transforms.removeNodes(this.editor, {
+          at: listItem[1]
+        })
+        const pre = Path.previous(listItem[1])
+        const [next] = Node.children(this.editor, pre, {reverse: true})
+        Transforms.insertNodes(this.editor, {
+          type: 'list',
+          order: list[0].order,
+          children: [{
+            type: 'list-item',
+            checked: Node.get(this.editor, pre).checked,
+            children: [{type: 'paragraph', children: [{text: ''}]}]
+          }]
+        }, {at: Path.next(next[1]), select: true})
+        return true
+      }
+      return false
     }
-    return false
   }
 }
