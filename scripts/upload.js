16a043b598bd1b869624b0cc1b9ee095df85a5e0
mdwriter-09idI <mdwriter@163.com>
2025-05-20 23:13:37 +0800

    chore: 2.0-test19

--- scripts/upload.js
+++ scripts/upload.js
@@ -2,6 +2,9 @@ const OSS = require('ali-oss')
 const path = require('path')
 const fs = require('fs')
 const os = require('os')
+const args = process.argv.slice(2)
+const platform = args[0]
+const arch = args[1]
 
 const client = new OSS({
   region: process.env.OSS_ENDPOINT,
@@ -10,36 +13,17 @@ const client = new OSS({
   bucket: process.env.OSS_BUCKET
 })
 
-const distPath = path.join(__dirname, '../dist')
-
-// 递归读取目录下的所有文件
-function getAllFiles(dirPath, arrayOfFiles = []) {
-  const files = fs.readdirSync(dirPath)
-
-  files.forEach((file) => {
-    const fullPath = path.join(dirPath, file)
-    if (fs.statSync(fullPath).isDirectory()) {
-      arrayOfFiles = getAllFiles(fullPath, arrayOfFiles)
-    } else {
-      arrayOfFiles.push(fullPath)
-    }
-  })
-
-  return arrayOfFiles
-}
+const distPath = path.join(__dirname, '../dist', platform, arch)
 
 async function uploadAllFiles() {
   try {
-    const files = getAllFiles(distPath)
+    const files = fs.readdirSync(dirPath)
 
     for (const file of files) {
       let relativePath = path.relative(distPath, file)
       // 只上传符合 release 定义的文件类型
       if (relativePath.match(/^Inkdown.*\..*$/) || relativePath.match(/^latest.*\.yml$/)) {
-        if (relativePath.startsWith('latest') && relativePath.endsWith('.yml')) {
-          relativePath = relativePath.split('.')[0] + '-' + os.arch() + '.yml'
-        }
-        const ossPath = `release/${relativePath}`
+        const ossPath = `release/${process.env.REF_NAME}/${platform}/${arch}/${relativePath}`
         await client.put(ossPath, file)
         console.log(`Uploaded: ${relativePath}`)
       }
