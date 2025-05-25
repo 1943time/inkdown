dc4f9eccf6a3effeaa4e347ee453e53b6f4a47ba
mdwriter-09idI <mdwriter@163.com>
2025-05-25 19:22:54 +0800

    chore: update electron-builder configuration and enhance afterPack script for better platform handling

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -9,24 +9,35 @@ exports.default = async function (context) {
   const onnBin = path.join(resorce, 'app.asar.unpacked/node_modules/onnxruntime-node/bin/napi-v3')
   console.log('onnBin', onnBin)
 
+  if (fs.existsSync(path.join(onnBin, 'linux'))) {
+    fs.rmSync(path.join(onnBin, 'linux'), { recursive: true, force: true })
+  }
   if (os.platform() === 'darwin') {
-    if (fs.existsSync(path.join(onnBin, 'linux'))) {
-      fs.rmSync(path.join(onnBin, 'linux'), { recursive: true, force: true })
-    }
     if (fs.existsSync(path.join(onnBin, 'win32'))) {
       fs.rmSync(path.join(onnBin, 'win32'), { recursive: true, force: true })
     }
-    if (fs.existsSync(path.join(onnBin, 'darwin'))) {
-      if (os.arch() === 'arm64') {
-        if (fs.existsSync(path.join(onnBin, 'darwin/x64'))) {
-          fs.rmSync(path.join(onnBin, 'darwin/x64'), { recursive: true, force: true })
-        }
-      } else {
-        if (fs.existsSync(path.join(onnBin, 'darwin/arm64'))) {
-          fs.rmSync(path.join(onnBin, 'darwin/arm64'), { recursive: true, force: true })
-        }
+    if (os.arch() === 'arm64') {
+      if (fs.existsSync(path.join(onnBin, 'darwin/x64'))) {
+        fs.rmSync(path.join(onnBin, 'darwin/x64'), { recursive: true, force: true })
+      }
+    } else {
+      if (fs.existsSync(path.join(onnBin, 'darwin/arm64'))) {
+        fs.rmSync(path.join(onnBin, 'darwin/arm64'), { recursive: true, force: true })
       }
     }
-  } else if (os.platform() === 'win32') {
+  }
+  if (os.platform() === 'win32') {
+    if (fs.existsSync(path.join(onnBin, 'darwin'))) {
+      fs.rmSync(path.join(onnBin, 'darwin'), { recursive: true, force: true })
+    }
+    if (os.arch() === 'arm64') {
+      if (fs.existsSync(path.join(onnBin, 'win32/x64'))) {
+        fs.rmSync(path.join(onnBin, 'win32/x64'), { recursive: true, force: true })
+      }
+    } else {
+      if (fs.existsSync(path.join(onnBin, 'win32/arm64'))) {
+        fs.rmSync(path.join(onnBin, 'win32/arm64'), { recursive: true, force: true })
+      }
+    }
   }
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -26,7 +26,6 @@ app.whenReady().then(async () => {
         }
       }
     })
-  createWindow()
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
